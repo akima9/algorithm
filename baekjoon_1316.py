f9c1387c14ce2c9152f1ad7df2d709a797e66eed
akima9 <akima9a@gmail.com>
2021-04-22 23:49:33 +0900

    그룹 단어 체커

--- baekjoon_1316.py
+++ baekjoon_1316.py
@@ -2,7 +2,6 @@ N = int(input())
 cnt = 0
 for i in range(N):
     word = input()
-    print(sorted(word,key=word.find))
     if list(word) == sorted(word,key=word.find):
         cnt += 1
 print(cnt)
