3508a5430f6dc116a5bac56bccad7fb7740da31f
akima9 <akima9a@gmail.com>
2021-05-12 22:55:24 +0900

    부녀회장이 될테야

---
+++ baekjoon_2775.py
@@ -0,0 +1,14 @@
+testCase = int(input())#테스트케이스
+
+for T in range(testCase + 1):
+    floor = int(input()) #층
+    room = int(input()) #호
+    zeroFloor = [] #0층
+    for i in range(1,room+1):
+        zeroFloor.append(i)
+    
+    for k in range(floor):
+        for n in range(1,room):
+            zeroFloor[n] += zeroFloor[n-1]
+    
+    print(zeroFloor[-1])
