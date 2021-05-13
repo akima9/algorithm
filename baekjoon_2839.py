1e45484e1ec3d45e8419f01a3cd784491f8bdc38
akima9 <akima9a@gmail.com>
2021-05-13 21:58:48 +0900

    설탕 배달

---
+++ baekjoon_2839.py
@@ -0,0 +1,14 @@
+N = int(input())
+cnt = 0
+while True:
+    if N % 5 == 0:
+        cnt += N//5
+        break
+    
+    N -= 3
+    cnt += 1
+
+    if N < 0:
+        cnt = -1
+        break
+print(cnt)
